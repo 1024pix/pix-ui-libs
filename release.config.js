03444832e2a95688358f4cbfa06b4bafe16904b0
Benjamin Petetot <benjamin.petetot@pix.fr>
2026-08-24 12:22:25 +0200

    [TECH] Configuration de la release

--- release.config.js
+++ release.config.js
@@ -1,56 +1,55 @@
+const pkgRoot = "./ember/nebulix";
+const changelogTitle = "@1024pix/ember-nebulix";
+
 module.exports = {
-  "branches": [
-    "dev",
-    "hotfix.*"
-  ],
-  "plugins": [
+  branches: ["dev", "hotfix.*"],
+  plugins: [
     // Analyse les commits pour definir la nouvelle version
     [
       "@semantic-release/commit-analyzer",
       {
-        "config": "@1024pix/conventional-changelog-pix",
-        "releaseRules": [
+        config: "@1024pix/conventional-changelog-pix",
+        releaseRules: [
           { revert: true, release: "patch" },
-          { tag: "BUGFIX", pr: '*', release: "patch" },
-          { tag: "BUMP", pr: '*', release: "patch" },
-          { tag: "DOC", pr: '*', release: "patch" },
-          { tag: "TECH", pr: '*', release: "patch" },
-          { tag: "FEATURE", pr: '*', release: "minor" },
-          { tag: "BREAKING", pr: '*', release: "major" },
+          { tag: "BUGFIX", pr: "*", release: "patch" },
+          { tag: "BUMP", pr: "*", release: "patch" },
+          { tag: "DOC", pr: "*", release: "patch" },
+          { tag: "TECH", pr: "*", release: "patch" },
+          { tag: "FEATURE", pr: "*", release: "minor" },
+          { tag: "BREAKING", pr: "*", release: "major" },
         ],
-      }
+      },
     ],
     // Génère un changelog à partir des commit de la release
     [
       "@semantic-release/release-notes-generator",
       {
-        "config": "@1024pix/conventional-changelog-pix"
-      }
+        config: "@1024pix/conventional-changelog-pix",
+      },
     ],
     // Modifie le fichier changelog
-    "@semantic-release/changelog",
+    ["@semantic-release/changelog", { changelogTitle }],
     // Publie la release dans github (avec le changelog)
     "@semantic-release/github",
     // Publie la release sur npm
-    [
-      "@anolilab/semantic-release-pnpm",
-      {
-        pkgRoot: './ember/nebulix',
-        publishBranch: 'dev'
-      }
-    ],
+    ["@anolilab/semantic-release-pnpm", { pkgRoot, publishBranch: "dev" }],
     // Crée le commit de release sur la branche principale
     [
       "@semantic-release/git",
       {
-        "assets": [
+        assets: [
           "CHANGELOG.md",
           "package.json",
           "package-lock.json",
-          ["**/package.json", "**/package-lock.json", "!**/node_modules/**/*.json"],
+          [
+            "**/package.json",
+            "**/package-lock.json",
+            "!**/node_modules/**/*.json",
+          ],
         ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
-  ]
-}
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
