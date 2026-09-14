0b3ef17deb64c370fd10fa8babf6f285d459ed16
LionelB <160320+lionelB@users.noreply.github.com>
2026-09-14 10:36:06 +0200

    docs(changeset): update PixTag to remove translations

--- ember/nebulix/src/translations/nl.js
+++ ember/nebulix/src/translations/nl.js
@@ -8,9 +8,6 @@ export default {
   select: {
     search: 'Zoeken',
   },
-  tag: {
-    removeButton: 'Verwijderen',
-  },
   stepper: {
     ariaLabel: 'Stap {current, number} van {total, number}',
   },
